02e1042ef5b234055677cd9135cd157447750975
Angelica <Angelica137@users.noreply.github.com>
2022-02-19 18:22:57 +0000

    add concatenate_rounds(), green

--- scripts/game.py
+++ scripts/game.py
@@ -1,2 +1,6 @@
 def get_rounds(round_number: int) -> list:
     return [round_number, round_number+1, round_number+2]
+
+
+def concatenate_rounds(rounds_1: list, rounds_2: list) -> list:
+    return rounds_1 + rounds_2
