226feb79c4e246626b849f06142e27bf45287e09
Angelica <Angelica137@users.noreply.github.com>
2022-02-19 18:15:11 +0000

    test concatenate_rounds(), red

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,5 +1,10 @@
+from numpy import concatenate
 from scripts.game import *
 
 
 def test_get_rounds_27():
     assert get_rounds(27) == [27, 28, 29]
+
+
+def test_concatenate_rounds_1():
+    assert concatenate_rounds([[27, 28, 29], [35, 36]]) == [27, 28, 29, 35, 36]
