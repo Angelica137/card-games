aa41808a0d5ce73a4b2ed8356190246e58270bd9
Angelica <Angelica137@users.noreply.github.com>
2022-02-19 17:30:30 +0000

    add get_rounds(), green

---
+++ scripts/game.py
@@ -0,0 +1,2 @@
+def get_rounds(round_number: int) -> list:
+    return [round_number, round_number+1, round_number+2]
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,2 +1,5 @@
+from scripts.game import *
+
+
 def test_get_rounds_27():
     assert get_rounds(27) == [27, 28, 29]
