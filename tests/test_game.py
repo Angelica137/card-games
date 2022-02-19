4675196d2c794e0654b1e5daee1f95cb8d39be11
Angelica <Angelica137@users.noreply.github.com>
2022-02-19 21:04:32 +0000

    test average_even_is_average_odd() returns true, red

--- tests/test_game.py
+++ tests/test_game.py
@@ -39,3 +39,7 @@ def test_approx_average_is_average_4_True():
 
 def test_approx_average_is_average_even_list_True():
     assert approx_average_is_average([1, 3, 3, 3]) == False
+
+
+def test_average_even_is_average_odd():
+    assert average_even_is_average_odd([1, 2, 3]) == True
